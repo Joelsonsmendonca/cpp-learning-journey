7025fe063085be65a7c471a973603fdfca9494ef
Joelson Dos Santos Mendonça junior <joelsonsantos181040@gmail.com>
2025-07-25 12:05:29 -0300

    Adicionar arquivos de projeto.

---
+++ Temperature converter/src/main.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+#include <map>
+
+int main()
+{
+	double celsius = 0.0;
+	double fahrenheit = 0.0;
+
+	enum escolha {
+		Sim,
+		Nao
+	};
+
+	escolha escolhaFinal = escolha::Nao;
+
+	int sairOuNao;
+
+	do {
+		std::cout << "Coloque a temperatura em celsius: ";
+		std::cin >> celsius;
+		fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+		std::cout << "A temperatura em fahrenheit eh: " << fahrenheit << std::endl;
+
+		std::cout << "Voc� deseja encerrar o programa? 0 para SIM e 1 para N�O" << std::endl;
+		std::cin >> sairOuNao;
+
+		if (sairOuNao == 0) {
+			escolhaFinal = escolha::Sim;
+		}
+		else if (sairOuNao == 1) {
+			escolhaFinal = escolha::Nao;
+		}
+		else {
+			std::cout << "Opção invalida. O programa será encerrado." << std::endl;
+		}
+
+		switch (escolhaFinal) {
+		case escolha::Sim:
+			break;
+
+		case escolha::Nao:
+			break;
+
+		}
+
+	} while (escolhaFinal == escolha::Nao);
+
+
+
+
+	return 0;
+}
