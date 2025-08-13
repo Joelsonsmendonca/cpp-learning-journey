ee77425ca4354b2776a878cd981053c76f0eb64a
Joelson Dos Santos Mendonça junior <joelsonsantos181040@gmail.com>
2025-08-13 12:31:36 -0300

    The bubble sort algorithm

---
+++ Bubble sort algorith/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <numeric>   // Para a função std::iota
+#include <random>    // Para o gerador de números aleatórios moderno
+#include <algorithm> // Para a função std::shuffle
+
+int main() {
+
+	std::vector<int> numbers = {1,4,5,7,1,4,7};
+	
+
+	bool trocou;
+
+
+	do {
+		trocou = false;
+
+		for (int i = 0; i < numbers.size() - 1; i++) {
+			if (numbers[i] > numbers[i + 1]) {
+
+				int aux = numbers[i];
+
+				numbers[i] = numbers[i + 1];
+
+				numbers[i + 1] = aux;
+
+				trocou = true;
+			}
+		}
+
+
+	} while (trocou);
+
+	for (int num : numbers) {
+
+		std::cout << num << " ";
+
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
