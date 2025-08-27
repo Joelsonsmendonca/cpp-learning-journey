2d466d06ebf4533105f6838cff316e34b7144dc8
Joelson Dos Santos Mendonça junior <joelsonsantos181040@gmail.com>
2025-08-27 15:30:28 -0300

    My first multiple object animation

--- Matrix rain/Matrix rain.cpp
+++ Matrix rain/Matrix rain.cpp
@@ -3,61 +3,73 @@
 #include <thread>    // Para o sleep
 #include <random>
 
-   struct Letra{
+   struct Letter{
     char character;
     int x;
     int y;
 } ;
+   void goToXY(int x, int y);
 
-// A única função nova que vamos aprender hoje
+int main() {
+
+    
+    const int SCREEN_WIDTH = 400;
+    const int SCREEN_HEIGHT = 200;
+
+    //configuração da aleatoriedade
+    std::random_device rd;
+    std::mt19937 rng(rd());
+
+    //gerar numeros nos intervalos desejados
+    std::uniform_int_distribution<int> distX(0, SCREEN_WIDTH - 1);
+    std::uniform_int_distribution<int> distY(0, SCREEN_HEIGHT - 1);
+    std::uniform_int_distribution<int> distChar(33, 126);
+
+    std::vector<Letter> allLetter;
+    const int NUM_STREAMS = 80;
+
+    for (int i = 0; i < NUM_STREAMS; ++i) {
+        Letter newLetter;
+
+        //give a aleatory caracter to newLetter
+        newLetter.character = static_cast<char>(distChar(rng));
+
+        //give a aleatory numY
+        newLetter.y = distY(rng);
+
+        newLetter.x = distX(rng);
+
+        allLetter.push_back(newLetter);
+    }
+
+    while (true)
+    {
+        system("cls");
+        for (auto& letter : allLetter) {
+            goToXY(letter.x, letter.y);
+
+            //print the current letter
+            std::cout << letter.character;
+
+            //update Y position for the next frame
+            letter.y++;
+
+            if (letter.y >= SCREEN_HEIGHT) {
+                letter.y = 0;
+            }
+        }
+        
+        //speed control
+        std::this_thread::sleep_for(std::chrono::milliseconds(60));
+
+    }
+
+
+}
+
 void goToXY(int x, int y) {
     COORD coord;
     coord.X = x;
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
-int main() {
-    Letra letra1;
-    letra1.character = 'a';
-
-    int y_position = 0;
-
-    std::random_device rd;
-    std::mt19937 rng(rd());
-    std::uniform_int_distribution<int> dist(1, 20);
-    
-    int randomNumber = dist(rng);
-    while (true) {
-        // Limpa a tela (um jeito simples)
-        system("cls");
-        
-        // Move o cursor para a posição (10, y_position)
-        const int NUM_STREAMS = 80;
-        for (int i = 0; i < NUM_STREAMS; i++) {
-            goToXY(randomNumber, y_position);
-
-        }
-        letra1.x = randomNumber;
-
-        // Imprime um caractere
-        std::cout << "j";
-        std::cout << "o";
-
-       
-        
-
-        // "Move" o caractere para baixo na próxima repetição
-        y_position++;
-
-        // Se chegar no final, volta para o topo
-        if (y_position > 29) {
-            y_position = 0;
-        }
-
-        // Espera um pouco para a animação não ser instantânea
-        std::this_thread::sleep_for(std::chrono::milliseconds(60));
-    }
-
-    return 0;
 }
