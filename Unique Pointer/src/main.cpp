ac4d98f617b6c2894f9b383681adfc308384d840
joelson dos santos mendonca junior <joelsonsantos181040@gmail.com>
2025-09-19 01:45:59 -0300

    Refactor unique pointer example to demonstrate ownership transfer and prevent dereferencing a null pointer

--- Unique Pointer/src/main.cpp
+++ Unique Pointer/src/main.cpp
@@ -4,5 +4,11 @@
 int main(){
     std::unique_ptr<int> intUniquePtr = std::make_unique<int>(5);
 
-    std::cout << *intUniquePtr  << std::endl;
+    //it does not allow copy, but allows move. the std::move transfers the ownership of the raw ponter internally to the other one.
+    std::unique_ptr<int> anotherUniquePtr = std::move(intUniquePtr);
+
+    while (true) {
+        std::cout << intUniquePtr.get() << std::endl;
+
+    }
 }
