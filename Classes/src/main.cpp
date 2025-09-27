853207450d04c1c03ab57e6135dc1b66ed9fb5f2
joelson dos santos mendonca junior <joelsonsantos181040@gmail.com>
2025-09-27 15:58:22 -0300

    Refactor Student class constructor and update main function to utilize constructor initialization

--- Classes/src/main.cpp
+++ Classes/src/main.cpp
@@ -4,25 +4,48 @@
 class Student{ 
 
 public: //anything under public is accessable out side.
+
+    //constructor is a function that has no ruteurn type, and has the same name as the class.
+    Student(const std::string& studentName, int studentID, int studentYear)
+            :name{studentName}, ID{studentID}, Year{studentYear} // more performant tha doing the assignement in the body of the contructor
+    {
+        //OR 
+        //name = studentName;
+        //ID  = studentID;
+        //Year = studentYear;
+
+        // this is a default constructor. if you do not provide any contructor, this will be automatically create.
+        //  if you do provide a constructor, then default constructor will not be created for you.
+
+
+    
+        std::cout << "Default contructor called"<< std::endl;
+    
+    }
+
     std::string name; //member variables
     int ID;
     int Year;
+private:
+    int secret;
 };
 
 
 int main(){
-    Student studentOne;
+    Student studentOne{"Sam", 0002, 1};
+    /*
     studentOne.name = "Sam";
     studentOne.ID = 0001;
     studentOne.Year = 01;
-    
+    */
     //std::cout << "The student name is: " << studentOne.name << " ID: " << studentOne.ID << " Year: " << studentOne.Year << std::endl;
 
-    Student studentTwo;
+    Student studentTwo{"Emily", 0002,02};
+    /*
     studentTwo.name = "Emily";
     studentTwo.ID = 0002;
     studentTwo.Year = 02;
-
+*/
     //std::cout << "The student name is: " << studentTwo.name << " ID: " << studentTwo.ID << " Year: " << studentTwo.Year << std::endl;
     
     std::vector<Student> students = {studentOne, studentTwo};
