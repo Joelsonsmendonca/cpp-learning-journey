9393b6e379884a17b9dfb86667661ff845c75421
joelson dos santos mendonca junior <joelsonsantos181040@gmail.com>
2025-09-27 15:09:19 -0300

    Add vector support and enhance Student class usage in main.cpp

--- Classes/src/main.cpp
+++ Classes/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Student{ 
 
@@ -12,8 +13,27 @@ public: //anything under public is accessable out side.
 int main(){
     Student studentOne;
     studentOne.name = "Sam";
-    studentOne.ID = 001;
+    studentOne.ID = 0001;
     studentOne.Year = 01;
     
-    std::cout << "The student name is: " << studentOne.name << " ID: " << studentOne.ID << " Year: " << studentOne.Year << std::endl;
+    //std::cout << "The student name is: " << studentOne.name << " ID: " << studentOne.ID << " Year: " << studentOne.Year << std::endl;
+
+    Student studentTwo;
+    studentTwo.name = "Emily";
+    studentTwo.ID = 0002;
+    studentTwo.Year = 02;
+
+    //std::cout << "The student name is: " << studentTwo.name << " ID: " << studentTwo.ID << " Year: " << studentTwo.Year << std::endl;
+    
+    std::vector<Student> students = {studentOne, studentTwo};
+
+    for(Student& student : students){
+
+        std::cout << "Student name: " << student.name << std::endl;
+        std::cout << "Student ID: " << student.ID << std::endl;
+        std::cout << "Student year" << student.Year << std::endl;
+
+    }
+
+
 }
