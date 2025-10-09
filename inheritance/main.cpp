dcf0f2014f7fdadb27201ac2c97f8610e9ca8dcf
joelson dos santos mendonca junior <joelsonsantos181040@gmail.com>
2025-10-09 20:20:09 -0300

    Refactor Vehicle and Car classes for clarity; implement pure virtual function in Vehicle and enhance Run method in Car

--- inheritance/main.cpp
+++ inheritance/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-class Vehicle{
+class Vehicle{ //it is an abstract concept.
 public:
     Vehicle(const std:: string& vehicleName, int vehicleCapacity)
         : name{vehicleName}, capacity{vehicleCapacity}
@@ -8,36 +8,50 @@ public:
 
     }
 
-    void Run()
-    {
-        std::cout << name << " With " << capacity << std::endl;
-    }
 
-private: // private is a really strong protect anyone child class have access.
-    int capacity;
+    //a pure virtual function. meaning the class is  abstract.
+    virtual void Run() = 0;
+
+    int GetCapacity() const { return capacity; } //getter function
+
+    void SetCapacity(int newCapacity){
+        if (newCapacity > 0) return;
+
+        capacity = newCapacity;
+    }
 
 protected: //Weak protection, the child class can access it, still not avaliable out side of the family
     std::string name; //and _ infront of the member indicates private.
+private:
+    int capacity;
 
 };
 
 class Car : public Vehicle // Car is inheriting from Vechile
 {
 public:
-    Car(const std::string& carName, int carCapacity)
-        :Vehicle{carName, carCapacity}
+    Car(const std::string& carName, int carCapacity, float carMileage)
+        :Vehicle{carName, carCapacity}, mileage{carMileage}
     {
 
     }
+    virtual void Run() override {
+        std::cout << name << " With " << mileage << "  mileage "<< GetCapacity() << " peaple running!" << "\n";
+    }
+
+    float GetMileage() const { return mileage; }
 
 private:
+    float mileage;
 
 };
 
 int main()
 {
-    Vehicle vehicleOne {"LightYears", 100000};
-    vehicleOne.Run(); 
-    Car carOne {"Corolla", 5};
+
+    //Vehicle vehicle {"sdiaojsdioa",213};  Abstract class is not allowed to create instances.
+    /*sVehicle vehicleOne {"LightYears", 100000}; Abstract class is not allowed to create instances.
+    vehicleOne.Run();*/ 
+    Car carOne {"Corolla", 5, 400000};
     carOne.Run();
 }
